d6dbdac965d460be18c0245bff6e4f8cd41d75fb
Dainis Abols <dainis@dainisabols.lv>
2019-03-19 00:50:49 +0200

    Added French translations

--- Lang/fr.lua
+++ Lang/fr.lua
@@ -6,24 +6,24 @@ Medusa.LangFr = {
     -- General --
     -------------------------------------------------------------------------------------------------
     Settings_General_Header = "General",
-    Settings_General_LockBar = "Locks Bars",
-    Settings_General_LockBar_Tooltip = "When OFF, bars are shown and unlocked and can be dragged around",
-    Settings_General_Trials = "Trials",
-    Settings_General_Trials_Description = "Here you can configure different trial settings. What you want or don't want to see and what size bars and colors you want to see.",
-    Settings_General_Width = "Width",
-    Settings_General_Width_Tooltip = "Adjusts the width of the bar",
-    Settings_General_Height = "Height",
-    Settings_General_Height_Tooltip = "Adjusts the height of the bar",
-    Settings_General_Color = "Color",
-    Settings_General_Color_Tooltip = "Changes the color of the bar",
+    Settings_General_LockBar = "Verouillée Barre",
+    Settings_General_LockBar_Tooltip = "Quand Off la barre afficher peut être bouger",
+    Settings_General_Trials = "raids",
+    Settings_General_Trials_Description = "Ici vous pouvez configuré les paramètres de raid. Ce que vous voulez voir la taille des barres et la couleurs que vous désirez voir.",
+    Settings_General_Width = "Largeur",
+    Settings_General_Width_Tooltip = " largeur de la barre",
+    Settings_General_Height = "Hauteur",
+    Settings_General_Height_Tooltip = "Hauteur de la barre",
+    Settings_General_Color = "Couleur",
+    Settings_General_Color_Tooltip = "Changer la couleur de la barre",
 
     -------------------------------------------------------------------------------------------------
     -- Trial: Cloudrest --
     -------------------------------------------------------------------------------------------------
-    Settings_Cloudrest = "Cloudrest",
-    Settings_Cloudrest_Tooltips = "Allows you to configure Coudrest settings",
+    Settings_Cloudrest = "Pas de la nuée",
+    Settings_Cloudrest_Tooltips = "Vous permet de changer les paramètres du pas de la nuée",
     Settings_Cloudrest_EndBoss1 = "Z'Maja",
-    Settings_Cloudrest_Kite = "Kite - Crushing Darkness",
-    Settings_Cloudrest_Kite_Show = "Show Kite Bar",
-    Settings_Cloudrest_Kite_Show_Tooltip = "When ON, the Kite bar will be shown and can be adjusted",
+    Settings_Cloudrest_Kite = "Kite- Ombres Ecrasante",
+    Settings_Cloudrest_Kite_Show = "Montre la barre de kite",
+    Settings_Cloudrest_Kite_Show_Tooltip = "Quand ON, la barre de kite afficher à l'écran pourra être ajusté et déplacer",
 }
